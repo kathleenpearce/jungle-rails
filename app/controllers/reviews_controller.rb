6e6dd64aab0aacf07d4a568f6128664ef833b58c
Kathleen pearce <kathleenpearce@live.com>
2019-05-18 00:46:27 +0000

    implemented review details (only author of review is allowed to delete, ect)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
 
   def create
   @product = Product.find params[:product_id]
@@ -10,6 +11,12 @@ class ReviewsController < ApplicationController
      redirect_to product_path(@product)
 
 end
+end
+
+def destroy
+  @review = Review.find params[:id]
+  @review.destroy
+  redirect_to product_path(@review.product)
 end
 
 private
